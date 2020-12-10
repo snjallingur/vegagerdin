84c2aa8f8ee6010f02250e17bb3cdd10456017da
snjallingur <70488552+snjallingur@users.noreply.github.com>
2020-12-10 19:39:52 +0000

    Create vegagerdin.py

---
+++ vegagerdin.py
@@ -0,0 +1,54 @@
+import json
+import requests
+import time
+import paho.mqtt.client as paho
+
+# MQTT Broker
+# IP address or host name
+broker="yourMQTTbroker"
+# create client and connect to broker
+client= paho.Client("client-001")
+client.connect(broker)
+
+urlfile = "http://gagnaveita.vegagerdin.is/api/faerd2017_1"
+
+#print(requests.get(urlfile).json())
+kjalarnes = {}
+hellisheidi = {}
+response = requests.get(urlfile).json()
+for IdButur in response:
+    #<IdButur>904210018</IdButur> -> Hvalfjarðarvegur. Botnsá - Hringvegur
+    #<IdButur>904000018</IdButur>
+    #<IdButur>904690018</IdButur> -> Hvalfjarðagöng
+    #<IdButur>902020018</IdButur> -> Hellisheiði
+    # Kjalarnes
+    if IdButur['IdButur'] == 904210018:
+        kjalarnes['condition'] = IdButur['AstandYfirbord']
+        kjalarnes['condition_description'] = IdButur['AstandLysing']
+        kjalarnes['condition_detail'] = IdButur['AstandVidbotaruppl']
+        kjalarnes['name'] = IdButur['StuttNafnButs']
+        kjalarnes['updated'] = IdButur['DagsKeyrtUt']
+        if IdButur['AstandYfirbord'] in ['LOKAD','OFAERT_ANNAD','OFAERT_VEDUR']:
+            kjalarnes['passable'] = "false"
+        else:
+            kjalarnes['passable'] = "true"              
+        #publish to MQTT
+        compact_obj = json.dumps(kjalarnes, separators=(',', ':'))
+        client.publish("homeassistant/vegagerdin/kjalarnes",compact_obj)
+    #
+    # Hellisheiði
+    if IdButur['IdButur'] == 902020018:
+        hellisheidi['condition'] = IdButur['AstandYfirbord']
+        hellisheidi['condition_description'] = IdButur['AstandLysing']
+        hellisheidi['condition_detail'] = IdButur['AstandVidbotaruppl']
+        hellisheidi['name'] = IdButur['StuttNafnButs']
+        hellisheidi['updated'] = IdButur['DagsKeyrtUt']
+        if IdButur['AstandYfirbord'] in ['LOKAD','OFAERT_ANNAD','OFAERT_VEDUR']:
+            hellisheidi['passable'] = "false"
+        else:
+            hellisheidi['passable'] = "true"    
+            
+        #publish to MQTT
+        compact_obj = json.dumps(hellisheidi, separators=(',', ':'))
+        client.publish("homeassistant/vegagerdin/hellisheidi",compact_obj)
+
